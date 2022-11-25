efc66ac4ba8f0d8a39d5195e8a62637d5d7d1833
manas <bagrimanasbir@gmail.com>
2022-11-24 21:44:45 -0800

    Update [committee].js

--- jhmun/pages/committees/[committee].js
+++ jhmun/pages/committees/[committee].js
@@ -4,7 +4,7 @@ import Link from 'next/link'
 import styles from '../../styles/Committess.module.scss'
 import committeeName from '../texts/committees.js';
 
-export default function committees() { 
+export default function Committees() { 
     const router = useRouter()
     const { committee } = router.query
 
