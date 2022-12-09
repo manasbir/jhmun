d5f522d47006367e8b97a7414a79b084070faaab
manas <bagrimanasbir@gmail.com>
2022-12-09 11:49:30 -0800

    a

--- jhmun/pages/_app.js
+++ jhmun/pages/_app.js
@@ -9,7 +9,17 @@ import "../styles/globals.scss";
 function MyApp({ Component, pageProps }) {
   const [isCommitteesHovered, setIsComHovered] = useState(false);
   const [isAboutHovered, setIsAboutHovered] = useState(false);
+  const [isMenuScroll, setIsMenuScroll] = useState(false)
 
+  if (typeof window !== "undefined") {
+    // Client-side-only code
+  function changeCss () {
+    var bodyElement = document.querySelector("body");
+    var navElement = document.querySelector("nav");
+    this.scrollY > 150 ? setIsMenuScroll(true) : setIsMenuScroll(false);
+  }
+    window.addEventListener("scroll", changeCss , false);
+  }
 
   const handleComHover = () => {
     if (isCommitteesHovered) {
@@ -42,45 +52,109 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <title>JHMUN</title>
         <meta name="description" content="Johnston Heights Model UN" />
-        <link rel="icon" href={"/design/Icons & Buttons/MUN EAGLE.svg"} />
+        <link rel="icon" href={"/design/Icons & Buttons/MUN Eagle White.svg"} />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="author" content="JHMUN" />
         <meta name="keywords" content="MUN, Model UN, Model United Nations, Johnston Heights, JH, Surrey, Surrey UN, BC, Fraser heights, Vancouver, Johntston Heights Secondary, IB Model UN"/>
       </Head>
-        <div className={styles.header}>
+        <div className={"header"}>
 
 
-      <div className={styles.logo}>
-          <Link href={"/"}>
-            <img src="/design/Icons & Buttons/MUN EAGLE.svg"/>
-          </Link>  
-        </div>    
-        <div className={styles.menu}>
-          <Link 
-          onMouseEnter={() => setIsComHovered(true)} 
-          onMouseLeave={() => setIsComHovered(false)}
-          href={"/committees"}>
-            COMMITTEES
-          {handleComHover()}
-          </Link>
+          <div className={styles.logo}>
+            <Link href={"/"}>
+              <img src={"/design/Icons & Buttons/MUN Eagle Green.svg"}/>
+            </Link>  
+          </div>    
+          <div className={"menu"}>
+            <Link 
+              onMouseEnter={() => setIsComHovered(true)} 
+              onMouseLeave={() => setIsComHovered(false)}
+              href={"/committees"}>
+                COMMITTEES
+              {handleComHover()}
+            </Link>
 
-          <Link 
-          onMouseEnter={() => setIsAboutHovered(true)} 
-          onMouseLeave={() => setIsAboutHovered(false)}
-          href={"/about"}>ABOUT
-          {handleAboutHover()}
-          </Link>
+            <Link 
+              onMouseEnter={() => setIsAboutHovered(true)} 
+              onMouseLeave={() => setIsAboutHovered(false)}
+              href={"/about"}>
+                ABOUT
+              {handleAboutHover()}
+            </Link>
 
-          <Link 
-          href={"/resources"}>
-            RESOURCES
-          </Link>
+            <Link 
+              href={"/resources"}>
+                RESOURCES
+            </Link>
 
-          <Link 
-          href={"/registration"}>
-            REGISTRATION
-          </Link>
-        </div>
+            <Link 
+              href={"/registration"}>
+                REGISTRATION
+            </Link>
+          </div>
+          <style jsx>{`
+
+            @keyframes fadeOut {
+              0% { color: ${isMenuScroll ? "rgba(0, 0, 0,1)": "rgba(255, 255, 255,1)"}; }
+              100% { color: ${isMenuScroll ? "rgba(0, 0, 0,.25)": "rgba(255, 255, 255,.25)"}; }
+            }
+
+            @keyframes fadeIn {
+              0% { color: ${isMenuScroll ? "rgba(0, 0, 0,.25)": "rgba(255, 255, 255,.25)"}; }
+              100% { color: ${isMenuScroll ? "rgba(0, 0, 0,1)": "rgba(255, 255, 255,1)"}; }
+            }
+            .header {
+              top: 0;
+              position: fixed;
+              padding: 1.5rem;
+              background-color: ${isMenuScroll ? "rgba(255, 255, 255, 1)" : "rgba(0, 0, 0, 0)"};
+              width: 100%;
+              display: flex;
+              flex-direction: row;
+              align-items: center;
+              justify-content: space-between;
+              border-radius: 0 0 1.25rem 1.25rem;
+              z-index: 10;
+            }
+            .menu {
+                animation: fadeIn 1s;
+                margin-right: 1rem;
+                margin-bottom: -0.5rem;
+                width: 30%;
+                align-items: center;
+                position: relative;
+                flex-direction: row;
+                display: flex;
+                justify-content: space-between;
+                color: white;
+                z-index: 10;
+
+              color: ${isMenuScroll ? "rgba(0,0,0,1)" : "rgba(255, 255, 255, 1)"};
+              text-decoration: none;
+              font-style: normal;
+              font-weight: 500;
+              font-size: 1.2rem;
+              text-align: center;
+              letter-spacing: 0.05em;
+            }
+
+            .menu:hover {
+              animation: fadeOut .25s forwards;
+            }
+
+            .menu :not(:hover) {
+              animation: fadeIn .5s forwards;
+            }
+
+            .menu a:hover {
+              animation: fadeIn .25s forwards;
+            }
+
+            .menu:hover a:not(:hover) {
+              animation: fadeOut .25s forwards;
+            }
+
+  `}</style>
         </div>
 
       <main className={styles.main}> 
--- jhmun/pages/committees/[committee].js
+++ jhmun/pages/committees/[committee].js
@@ -13,6 +13,7 @@ export default function Committees() {
     console.log(committee);
     let names = [];
     let images = [];
+    let images2 = [];
     let descriptions = [];
     let image;
     let image2;
@@ -21,13 +22,15 @@ export default function Committees() {
         title = "Historical Crisis Committee";
         names = ["Manasbir Bagri | Director", "ma friend | CO-DIRECTOR"];
         images = ["/design/Images/people/manas.png", "/design/Images/gj.png"];
+        images2 = ["/design/Images/Text backgrounds.png", "/design/Images/Text backgrounds.png"];
         descriptions = ["wassup guys. coolest guy here ong. i love pizza, touching grass etc", "hello madam"]
-        image = "/design/Images/Soviet Union Committees Background.png"
-        image2 = "/design/Images/Soviet Union background.png"
+        image = "/design/Images/Dark Berline Wall.png"
+        image2 = "/design/Images/Green Berlin.png"
     } else if (committee == "sochum") {
         title = "Social Humanitarian Cultural Committee";
         names = ["Manasbir Bagri | Director", "ma friend | CO-DIRECTOR"];
         images = ["/design/Images/people/manas.png", "/design/Images/gj.png"];
+        images2 = ["/design/Images/Text backgrounds.png", "/design/Images/Text backgrounds.png"];
         descriptions = ["wassup guys. coolest guy here ong. i love pizza, touching grass etc", "hello madam"]
         image = "/design/Images/Rohingya Committees Background.png";
         image2 = "/design/Images/Rohingya Muslims background.png";
@@ -35,6 +38,7 @@ export default function Committees() {
         title = "United Nations Office on Drugs and Crime";
         names = ["Manasbir Bagri | Director", "ma friend | CO-DIRECTOR"];
         images = ["/design/Images/people/manas.png", "/design/Images/gj.png"];
+        images2 = ["/design/Images/Text backgrounds.png", "/design/Images/Text backgrounds.png"];
         descriptions = ["wassup guys. coolest guy here ong. i love pizza, touching grass etc", "hello madam"]
         image = "/design/Images/Doctors Committee Background.png";
         image2 = "/design/Images/MUN doctors background.png";
@@ -42,6 +46,7 @@ export default function Committees() {
         title = "United Nations Development Programme";
         names = ["Manasbir Bagri | Director", "ma friend | CO-DIRECTOR"];
         images = ["/design/Images/people/manas.png", "/design/Images/gj.png"];
+        images2 = ["/design/Images/Text backgrounds.png", "/design/Images/Text backgrounds.png"];
         descriptions = ["wassup guys. coolest guy here ong. i love pizza, touching grass etc", "hello madam"]
         image = "/design/Images/Palestine Israel Committees Background.png";
         image2 = "/design/Images/Palestine Israel Committees Background.png";
@@ -52,13 +57,36 @@ export default function Committees() {
         let staff = [];
         for (let i = 0; i < names.length; i++) {
             staff.push(
-            <div className={styles.card}>
-                <div>
-                    <img src={images[i]}/>
-                </div>
+            <div className={"card"}>
+                <style jsx>{`
+                    .card {
+                        width: 70vw;
+                        position: relative;
+                        display: flex;
+                        flex-direction: row;
+                        justify-content: space-between;
+                        padding: 5%;
+                        text-align: left;
+                        background: url("${images2[i]}");  
+                        background-position: center;
+                        background-size: auto 100%;
+                        background-attachment: fixed;
+                        margin-top: 2rem;
+                        border-radius: 1rem;
+                    }
+                    img {
+                        width: 20vw;
+                        object-fit: cover;
+                    }
+                    h1 {
+                        text-align: left;
+                        font-size: 2rem;
+                    }
+                `}</style>
+                <img src={images[i]}/>
                 <div className={styles.cardText}>
-                <h1>{names[i]}</h1>
-                <p>{descriptions[i]}</p>
+                    <h1>{names[i]}</h1>
+                    <p>{descriptions[i]}</p>
                 </div>
             </div>
             )
@@ -92,14 +120,12 @@ export default function Committees() {
 
                     .background {
                         margin-top: -1.25rem;
-                        background-color: #1A3A16;
                         animation: loadIn2 2s;
                         z-index: 0;
                         
                         position: relative;
                         width: 100vw;
                         
-                        color: $white;
                         flex-direction: column;
                         display: flex;
                         align-items: center;
@@ -131,18 +157,18 @@ export default function Committees() {
                 <div className={"text"}>
                     <style jsx>{`
                     .text {
+                        margin-top: 2rem;
                         background-image: url("${image2}");
                         background-position: center;
                         background-size: cover;
                         background-repeat: no-repeat;
                         background-attachment: fixed;
                         position: relative;
-                        left: 15%;
                         width: 70%;
-                        color: $white;
-                        align-items: center;
+                        color: white;
                         display: flex;
                         flex-direction: column;
+                        align-items: center;
                         border-radius: 1rem;
                       }
                       h1 {
@@ -153,7 +179,7 @@ export default function Committees() {
                         font-weight: 700;
                         font-size: 1.75rem;
                         line-height: 40px;
-                        font-family: "Din Condensed", "Lato";
+                        font-family: "Roboto";
                       }
 
                       p {
@@ -163,20 +189,14 @@ export default function Committees() {
                         font-weight: 300;
                         font-size: 1.1rem;
                         line-height: 35px;
-                        font-family: "Lato";
+                        font-family: "Roboto";
                         padding-bottom: 18px;
                       }
 
-                      hr {
-                        width: 100%;
-                        border: 1px solid $white;
-                        }
-
-                      ::selection {
+                      .text::selection {
                         background-color: $primary-color;
-                        color: $white;
+                        color: white;
                       }
-
                     `}</style>
                     <h1>Committee Description:</h1>
                     {text}
@@ -185,11 +205,7 @@ export default function Committees() {
                     <Link href={`/committess/${committee}/backgrounder`}><button>BACKGROUNDER</button></Link>
                     </div>
                 </div>
-            <div className={styles.background}>
-            {staff()}
-            </div>
-
-                
+                {staff()}
             </div>
         </div>
     )
